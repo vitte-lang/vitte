302e8dbaf25f4033f6ab4c8dc6a282038879e04a
Vincent Rousseau <vincentrspro@gmail.com>
2026-07-18 07:34:07 +0200

    Remove retired bootstrap stage scripts

--- toolchain/scripts/bootstrap/stage1.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-echo "[stage1][error] legacy stage1 bootstrap is disabled; vittec0.seed is the only trust root" >&2
-exit 1
--- toolchain/scripts/bootstrap/stage2.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-echo "[stage2][error] legacy stage2 bootstrap is disabled; vittec0.seed is the only trust root" >&2
-exit 1
